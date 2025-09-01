27a8d4ba878a708d88952f2c1333f1295ff0d675
J Hendrix <j.d.hendrix@outlook.com>
2025-09-01 16:44:31 -0400

    Quartz sync: Sep 1, 2025, 4:44 PM

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
-del "C:\Users\Johnny\Documents\2 RPGs\Quartz\quartz\content"
+del /q "C:\Users\Johnny\Documents\2 RPGs\Quartz\quartz\content"
 xcopy "C:\Users\Johnny\Documents\2 RPGs\Obsidian\Antir" "C:\Users\Johnny\Documents\2 RPGs\Quartz\quartz\content" /E /Y
 npx quartz sync
