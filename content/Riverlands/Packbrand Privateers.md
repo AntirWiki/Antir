48b2d503a417d0b5878a353eaa0795a4fc8a734e
J Hendrix <j.d.hendrix@outlook.com>
2025-09-01 16:47:27 -0400

    Quartz sync: Sep 1, 2025, 4:47 PM

--- content/Riverlands/Packbrand Privateers.md
+++
@@ -1,6 +0,0 @@
----
-tags:
-  - group
----
-![[Corsair Molossus.png|700]]
-The Packbrand Privateers are a naval organization founded by [[Valerie Bouchard|Corsair Molossus Valerie Bouchard]].
--- update.bat
+++ update.bat
@@ -1,3 +1,4 @@
-del /q "C:\Users\Johnny\Documents\2 RPGs\Quartz\quartz\content"
+rd /s /q "C:\Users\Johnny\Documents\2 RPGs\Quartz\quartz\content"
+mkdir content
 xcopy "C:\Users\Johnny\Documents\2 RPGs\Obsidian\Antir" "C:\Users\Johnny\Documents\2 RPGs\Quartz\quartz\content" /E /Y
 npx quartz sync
