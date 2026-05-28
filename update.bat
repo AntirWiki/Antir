5602d0260ee6f5c68f6e1a25cd6666dc2ea3cebf
J Hendrix <j.d.hendrix@outlook.com>
2026-05-27 22:33:16 -0400

    Quartz sync: May 27, 2026, 10:33 PM

--- update.bat
+++ update.bat
@@ -2,3 +2,4 @@ rd /s /q "C:\Users\Johnny\Documents\2 RPGs\Quartz\quartz\content"
 mkdir content
 xcopy "C:\Users\Johnny\Documents\2 RPGs\Obsidian\Antir" "C:\Users\Johnny\Documents\2 RPGs\Quartz\quartz\content" /E /Y
 npx quartz sync
+pause
